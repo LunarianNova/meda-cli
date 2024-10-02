e11c9b30bee382fc36230171477b833229820c85
LunarianNova <lunariannova@gmail.com>
2024-10-02 15:57:09 -0400

    Comments

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class Inputs:
     CTRL_O = 15
     CTRL_A = 1
     CTRL_X = 24
+    OVERRIDES = [CTRL_I, CTRL_O, CTRL_A, CTRL_X]
 
 class FileEditor:
     def __init__(self, file: str=""):
@@ -20,18 +21,22 @@ class FileEditor:
         ...
 
     def run(self) -> None:
+        """
+            Sets the terminal to no echo, and process user input
+            without waiting for return. Opens the FileEditor window
+        """
         try:
             self.running = True
             curses.noecho()
             curses.cbreak()
-            self.scr.keypad(True)
+            self.scr.keypad(True) # Clears window
             while self.running:
                 self.handle_input()
-        except KeyboardInterrupt:
+        except KeyboardInterrupt: # Control+C
             self.close()
-        except Exception:
+        except Exception: # Genuine Error
             self.close()
-            print(traceback.format_exc())
+            print(traceback.format_exc()) # Print the stack trace
 
     def close(self) -> None:
         """
